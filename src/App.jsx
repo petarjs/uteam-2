7dd059e41ad9391db28da481f33cb6defd9820b8
mihajlo-cirikovacki <cirikovackimihajlo@yahoo.com>
2022-01-14 22:38:43 +0100

    Added protect-auth-routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { Fragment, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 import './App.scss';
 
@@ -9,43 +9,70 @@ import PendingApproval from './components/features/PendingApproval/PendingApprov
 import Register from './components/features/Register/Register.jsx';
 import AsideMenu from './components/Layout/AsideMenu/AsideMenu.jsx';
 import CompanyInfo from './components/Layout/CompanyInfo/CompanyInfo.jsx';
+import MainContent from './components/Layout/MainContent/MainContent.jsx';
 import MyProfile from './components/Layout/MyProfile/MyProfile.jsx';
 import Questions from './components/Layout/Questions/Questions.jsx';
 import Team from './components/Layout/Team/Team.jsx';
 import TopBar from './components/Layout/Topbar/TopBar.jsx';
-import { AuthContextProvider } from './context/AuthContext.jsx';
+import ProtectedRoute from './components/protectedRoute/ProtectedRoute.jsx';
 
-import MainContent from 'components/Layout/MainContent/MainContent.jsx';
 import { useAuthContext } from 'context/AuthContext.jsx';
 
 function App() {
-  // eslint-disable-next-line no-unused-vars
-  // const { isLoggedIn } = useAuthContext();
+  const { isUserLoggedIn } = useAuthContext();
+
   return (
-    <BrowserRouter>
-      <AuthContextProvider>
-        {/* {isLoggedIn && <TopBar />}
-        {isLoggedIn && <AsideMenu />} */}
-        <TopBar />
-        <AsideMenu />
-        <MainContent>
-          <Routes>
-            <Route path="/" element={<PendingApproval />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/logout" element={<Login />} />
-            <Route path="/my-profile" element={<MyProfile />} />
-            <Route path="/company-info" element={<CompanyInfo />} />
-            <Route path="/questions" element={<Questions />} />
-            <Route path="/team" element={<Team />} />
-            <Route path="/pending-approval" element={<PendingApproval />} />
-            <Route path="/my-profile" element={<MyProfile />} />
-          </Routes>
-        </MainContent>
-      </AuthContextProvider>
-    </BrowserRouter>
+    <Fragment>
+      {isUserLoggedIn && <TopBar />}
+      {isUserLoggedIn && <AsideMenu />}
+      <MainContent>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/pending-approval"
+            element={
+              <ProtectedRoute>
+                <PendingApproval />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/my-profile"
+            element={
+              <ProtectedRoute>
+                <MyProfile />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/company-info"
+            element={
+              <ProtectedRoute>
+                <CompanyInfo />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/questions"
+            element={
+              <ProtectedRoute>
+                <Questions />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/team"
+            element={
+              <ProtectedRoute>
+                <Team />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </MainContent>
+    </Fragment>
   );
 }
 
 export default App;
-// style={{ padding: ' 0 15rem ' }}
