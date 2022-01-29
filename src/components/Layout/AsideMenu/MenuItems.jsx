8002f32f14a0776e9b85193f60029a8d8f1ebf69
Borovnica1 <petkovic.czv1@gmail.com>
2022-01-29 13:29:58 +0100

    on route change highlight menu link on aside menu of page we are currently viewing

--- src/components/Layout/AsideMenu/MenuItems.jsx
+++ src/components/Layout/AsideMenu/MenuItems.jsx
@@ -1,15 +1,26 @@
 import { Icon, Text } from '@chakra-ui/react';
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { AiOutlineTeam } from 'react-icons/ai';
 import { MdOutlinePending } from 'react-icons/md';
 import { RiQuestionnaireLine, RiInformationLine, RiProfileLine } from 'react-icons/ri';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import MenuLink from './MenuLink';
 import MenuWrap from './MenuWrap';
 
 function MenuItems({ menuType }) {
+  const itemsRef = useRef([]);
+  const [activeLink, setActiveLink] = useState(0);
   let menuItems;
+  const location = useLocation();
+
+  useEffect(() => {
+    // runs on location, i.e. route, change
+    console.log('handle route change here', location);
+    menuItems.forEach((item, index) => {
+      if (item.href === location.pathname) setActiveLink(index);
+    });
+  }, [location, menuItems]);
 
   switch (menuType) {
     default:
@@ -47,8 +58,12 @@ function MenuItems({ menuType }) {
     <MenuWrap menuType={menuType}>
       {menuItems.map(({ text, icon, href }, i) => (
         <Link to={href} key={i}>
-          <MenuLink menuType={menuType}>
-            <Text fontSize="2xl">
+          <MenuLink
+            menuType={menuType}
+            innerRef={(el) => (itemsRef.current[i] = el)}
+            active={i == activeLink ? true : false}
+          >
+            <Text fontSize="2xl" ml="1rem">
               <Icon as={icon} mr="1rem" mt=".2rem"></Icon>
               {text}
             </Text>
